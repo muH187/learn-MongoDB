6fd1b654b878d1a3e766fa3a47cba5cd0b5ff7a2
Muhammad Ali <proqualitythings100@gmail.com>
2024-06-11 22:30:49 +0500

    ejs crud

--- CRUD-EJS/app.js
+++ CRUD-EJS/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const userModel = require('./models/user')
+
 
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -12,6 +14,15 @@ app.get('/', (req, res) => {
 })
 app.get('/read', (req, res) => {
     res.render("read")
+})
+app.post('/create', async (req, res) => {
+    let {name, email, image} = req.body 
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.send(createdUser)
 })
 
 app.listen(3000)
