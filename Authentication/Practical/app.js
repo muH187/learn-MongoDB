a8d3649419562985344789061185954bbc311dc1
Muhammad Ali <proqualitythings100@gmail.com>
2024-07-03 13:03:51 +0500

    authorization

---
+++ Authentication/Practical/app.js
@@ -0,0 +1,35 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const userModel = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.post("/create", (req, res) => {
+  let { username, email, password, age } = req.body;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let createdUser = await userModel.create({
+        username,
+        email,
+        password: hash,
+        age,
+      });
+      res.send(createdUser);
+    });
+  });
+});
+
+app.listen(3000);
